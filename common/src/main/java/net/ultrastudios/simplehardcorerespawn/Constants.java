c4292af5c392077ca10c45a6ba258eb64f2bc2b4
Bles-1 <blazejradczyc@gmail.com>
2025-08-24 20:06:36 +0200

    Initial commit for multi-loader template.

---
+++ common/src/main/java/net/ultrastudios/simplehardcorerespawn/Constants.java
@@ -0,0 +1,11 @@
+package net.ultrastudios.simplehardcorerespawn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "simple_hardcore_respawn";
+	public static final String MOD_NAME = "Simple Hardcore Respawn";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
