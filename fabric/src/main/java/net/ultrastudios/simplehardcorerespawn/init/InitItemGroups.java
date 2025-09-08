00c2f6a20f7a868943d60506b268c1d3745cb108
Bles-1 <blazejradczyc@gmail.com>
2025-09-08 23:29:38 +0200

    Multi-loader setup finished.

---
+++ fabric/src/main/java/net/ultrastudios/simplehardcorerespawn/init/InitItemGroups.java
@@ -0,0 +1,13 @@
+package net.ultrastudios.simplehardcorerespawn.init;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.ultrastudios.simplehardcorerespawn.Constants;
+import org.jetbrains.annotations.NotNull;
+
+public class InitTabs {
+
+}
